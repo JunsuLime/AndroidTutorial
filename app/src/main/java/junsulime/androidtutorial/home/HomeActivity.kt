ced54b16808f10d2ab89b61553c29fc172ad518c
junsulime <vfv0121@gmail.com>
2018-12-14 20:11:37 +0900

    feat: Add view components of SignActivity.
    TODO: keep session/cookie on http header.

---
+++ app/src/main/java/junsulime/androidtutorial/home/HomeActivity.kt
@@ -0,0 +1,11 @@
+package junsulime.androidtutorial.home
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+class HomeActivity: AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
